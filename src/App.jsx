55e105abc91c3563b166df041ee8ca2477fc22e2
Rodrigo(Zara) <rodzmaciel21@gmail.com>
2025-09-21 19:51:36 -0300

    Atualizando projeto com novas melhorias

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Navbar from './Components/Navbar';
-import Hero from './Components/Hero';
-import Sobre from './Components/Sobre';
-import Portfolio from './Components/Portfolio';
-import Contato from './Components/Contato';
+import Navbar from './Components/Navbar.jsx';
+import Hero from './Components/Hero.jsx';
+import Sobre from './Components/Sobre.jsx';
+import Portfolio from './Components/Portfolio.jsx';
+import Contato from './Components/Contato.jsx';
 import './index.css';
 
 const App = () => (
